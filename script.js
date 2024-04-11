9adb18e94c85864fc999d10a61b5f3b801d1f9a4
harryGit8 <harry.ortega6148@mycanctb.ca>
2024-04-10 21:22:58 -0700

    VariableandConstantJS

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 //Activity 1
 
-alert("Hello World");
+//alert("Hello World");
+
+//Activity 2
+
+let vName = "Harry Ortega";
+let vHeight = 1.74;
+let vIsLive = true;
+let vAge = 35;
+
+const placeBirth = "Manta";
