6b7910938fa709e11befa8327f6ed08cf87b81c6
harryGit8 <harry.ortega6148@mycanctb.ca>
2024-04-10 21:40:08 -0700

    activity2

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ let vIsLive = true;
 let vAge = 35;
 
 const placeBirth = "Manta";
+
+console.log("Person Name is " + vName);
+console.log("Height is " + vHeight);
+console.log("Is he Live? is " + vIsLive);
+console.log("His age is " + vAge);
+console.log("He was born in " + placeBirth + " city");
